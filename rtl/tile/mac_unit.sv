6ad67384fc3ee3e7cccc98098cfe0859b87a5d79
ethanh8 <ethanhung720@gmail.com>
2026-06-22 12:40:00 +0800

    pipelined MAC operation for DSP tiles

--- rtl/tile/mac_unit.sv
+++ rtl/tile/mac_unit.sv
@@ -13,11 +13,20 @@ module mac_unit #(
     input logic en,
     output logic [ACC_W-1:0] P
 );
+  logic [2:0] en_d, clr_d; 
+  logic [DATA_W-1:0] a_r1, a_r2, b_r1, b_r2;
+  logic [ACC_W-1:0] P_r;
 
   always_ff @(posedge clk) begin
+    a_r1 <= a; a_r2 <= a_r1; b_r1 <= b; b_r2 <= b_r1;
+    P_r <= a_r2 * b_r2;
+
+    en_d[0] <= en; en_d[1] <= en_d[0]; en_d[2] <= en_d[1];
+    clr_d[0] <= clr; clr_d[1] <= clr_d[0]; clr_d[2] <= clr_d[1];
+
     if (rst) P <= '0;
-    else if (clr) P <= '0;
-    else if (en) P <= a * b + P;
+    else if (clr_d[2]) P <= '0;
+    else if (en_d[2]) P <= P_r + P;
   end
 
 endmodule
